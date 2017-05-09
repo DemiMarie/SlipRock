b678920699d88e80745a7c86e1cd1c7ef429f01e
Demi Obenour <demiobenour@gmail.com>
2017-05-09 16:30:52 -0400

    Use -Werror in YouCompleteMe

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -35,37 +35,25 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Wstrict-prototypes',
-'-Wmissing-prototypes',
-'-Wmissing-declarations',
-'-Winline',
-'-Waggregate-return',
-'-Wpointer-arith',
-'-Wmissing-noreturn',
-'-Wnested-externs',
-'-Wredundant-decls',
 '-Weverything',
+'-Werror',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
 # a "-std=<something>".
 # For a C project, you would set this to something like 'c99' instead of
 # 'c++11'.
-'-std=gnu99',
+'-std=c89',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c',
-'-I', 'src',
+'-x', 'c', '-Isrc', '-Iinclude'
 #'-I', '/usr/x86_64-w64-mingw32/sys-root/mingw/include/',
 ]
 def DirectoryOfThisScript():
   return os.path.dirname( os.path.abspath( __file__ ) )
-compilation_database_folder = os.path.join(DirectoryOfThisScript(), '../build-sliprock-windows')
+compilation_database_folder = os.path.join(DirectoryOfThisScript(), '../build-sliprock')
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
