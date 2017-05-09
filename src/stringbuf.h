7f4f08aa43cf5a516565087089383744ce79b8f0
Demi Obenour <demiobenour@gmail.com>
2017-05-09 17:55:28 -0400

    Add include of <string.h> to stringbuf.h

--- src/stringbuf.h
+++ src/stringbuf.h
@@ -1,3 +1,5 @@
+#ifndef SLIPROCK_STRINGBUF_H_INCLUDED
+#define SLIPROCK_STRINGBUF_H_INCLUDED SLIPROCK_STRINGBUF_H_INCLUDED
 #ifdef _WIN32
 #define TCHAR wchar_t
 #else
@@ -7,6 +9,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 #ifdef NDEBUG
 #undef NDEBUG
 #endif
@@ -17,9 +20,10 @@ struct StringBuf {
   TCHAR buf[];
 };
 
-static inline struct StringBuf *StringBuf_alloc(size_t const buf_capacity) {
+static inline struct StringBuf *
+StringBuf_alloc(size_t const buf_capacity) {
   assert(buf_capacity < UINT16_MAX);
-  struct StringBuf val = (struct StringBuf *)calloc(
+  struct StringBuf *val = (struct StringBuf *)calloc(
       sizeof(struct StringBuf) + buf_capacity * sizeof(TCHAR) + 1, 1);
   if (NULL == val)
     return NULL;
@@ -34,7 +38,7 @@ static inline void StringBuf_add_char(struct StringBuf *buf, TCHAR c) {
 static inline void StringBuf_add_decimal(struct StringBuf *buf,
                                          uintptr_t value) {
   assert(buf->buf_capacity - buf->buf_length >= 20);
-  int oldlen = buf->buf_length;
+  // int oldlen = buf->buf_length;
   int cap = 0;
   uintptr_t currentval = value;
   do {
@@ -50,7 +54,8 @@ static inline void StringBuf_add_decimal(struct StringBuf *buf,
   } while (value > 0);
 }
 
-static inline void StringBuf_add_hex(struct StringBuf *buf, uint64_t value) {
+static inline void StringBuf_add_hex(struct StringBuf *buf,
+                                     uint64_t value) {
   assert(buf->buf_capacity - buf->buf_length >= 8);
   for (int i = 0; i < 64; i += 4) {
     int res = (value >> i & 0xF);
@@ -58,11 +63,24 @@ static inline void StringBuf_add_hex(struct StringBuf *buf, uint64_t value) {
   }
 }
 
-static inline void StringBuf_add_string(struct StringBuf *buf, const char *ptr,
-                                        size_t len) {
-  assert(buf->buf_capacity - buf->buf_length >= len);
-  for (uint16_t i = buf->buf_length, j = buf->buf_length + len; i < j; ++i) {
+static inline void StringBuf_add_string(struct StringBuf *buf,
+                                        const TCHAR *ptr, size_t len) {
+  assert((size_t)(buf->buf_capacity - buf->buf_length) >= len);
+  for (uint16_t i = buf->buf_length, j = buf->buf_length + len; i < j;
+       ++i) {
     buf->buf[i] = *ptr++;
   }
-  buffer->buf_length += len;
+  buf->buf_length += len;
 }
+
+static inline void StringBuf_add_literal(struct StringBuf *buf,
+                                         const char *ptr) {
+  const size_t len = strlen(ptr);
+  assert((size_t)(buf->buf_capacity - buf->buf_length) >= len);
+  for (uint16_t i = buf->buf_length, j = buf->buf_length + len; i < j;
+       ++i) {
+    buf->buf[i] = *ptr++;
+  }
+  buf->buf_length += len;
+}
+#endif //! defined SLIPROCK_STRINGBUF_H_INCLUDED
