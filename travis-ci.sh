d70b6c92ab351e36a00659e3a71ad9e29879058d
Demi Obenour <demiobenour@gmail.com>
2017-05-18 16:02:18 -0400

    Add Travis CI scripts

---
+++ travis-ci.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -ex
+set | sort
+mkdir build-gtest
+cd build-gtest
+curl -o release-1.8.0.zip \
+   https://github.com/google/googletest/archive/release-1.8.0.zip
+unzip build-gtest
+cmake -DCMAKE_PREFIX_PATH=.. .
+make
+make install
+cd ..
+mkdir build
+cd build
+cmake ..
+make
+make test
