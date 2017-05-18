a99438f6f9badaa8b7d19011f9a3ead193ae56bd
Demi Obenour <demiobenour@gmail.com>
2017-05-18 16:13:10 -0400

    Tell curl to follow redirects

--- travis-ci.sh
+++ travis-ci.sh
@@ -3,9 +3,9 @@ set -ex
 set | sort
 mkdir build-gtest
 cd build-gtest
-curl -o release-1.8.0.zip \
+curl -L -o release-1.8.0.zip \
    https://github.com/google/googletest/archive/release-1.8.0.zip
-unzip -d build-gtest release-1.8.0.zip
+unzip release-1.8.0.zip
 cmake -DCMAKE_PREFIX_PATH=.. .
 make
 make install
