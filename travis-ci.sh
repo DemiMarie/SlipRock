73555ced1637a9b63d3ca8033ed22b1c521422ac
Demi Obenour <demiobenour@gmail.com>
2017-05-22 14:56:02 -0400

    Switch Travis scripts

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env sh
 set -ex
 set | sort
-curl -L -o release-1.8.0.zip \
-   https://github.com/google/googletest/archive/release-1.8.0.zip
-unzip release-1.8.0.zip
-cd googletest-release-1.8.0
-cmake -DCMAKE_INSTALL_PREFIX=.. .
-make
-make install
-cd ..
 mkdir build
 cd build
 cmake ..
