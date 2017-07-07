f0fd9e698c941755fd62b9636cea29caa9d20c44
Demi Obenour <demiobenour@gmail.com>
2017-07-07 15:24:29 -0400

    Make Travis CI script work from any dir

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
+case $0 in
+    */*) cd "${0%/*}/";;
+esac
 set -ex
 set | sort
 rm -rf build
 mkdir build
 cd build
+cc=cc
 if command -v ninja; then
-    cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug
+    cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER="$cc"
     ninja
 else
-    cmake .. -DCMAKE_BUILD_TYPE=Debug
+    cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER="$cc"
     make
 fi
 src/mytest
