31fcfb036fff67c3a6b1fa0ec4cfe77f0d4b0cd6
Demi Obenour <demiobenour@gmail.com>
2017-07-16 14:30:29 -0400

    Fix intermittent hang on Windows
    It is absolutely imperative that there be ONLY ONE instance of the named pipe
    open but not connected when ConnectNamedPipe() is called.  Otherwise, Windows
    will pick an arbitrary one.  This causes ConnectNamedPipe() to hang
    intermittently, causing problems that are terribly difficult to debug!
    
    The solution is to wait until ConnectNamedPipe() returns to make the next
    CreateNamedPipe() call.  This bug took a very long time to fix.

--- src/sliprock_windows.h
+++ src/sliprock_windows.h
@@ -35,18 +35,17 @@ typedef HANDLE OsHandle;
 static void sliprock_strerror(void) {
   wchar_t *buf;
   DWORD dummy;
-  DWORD buflen = FormatMessageW(
-      FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS |
-          FORMAT_MESSAGE_FROM_SYSTEM,
-      NULL, GetLastError(), 0, (LPTSTR)&buf, 0, NULL);
+  DWORD buflen = FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+                                    FORMAT_MESSAGE_IGNORE_INSERTS |
+                                    FORMAT_MESSAGE_FROM_SYSTEM,
+                                NULL, GetLastError(), 0, (LPTSTR)&buf, 0, NULL);
   if (buflen) {
     if (!WriteConsoleW(GetStdHandle(-12), buf, buflen, &dummy, NULL)) {
       size_t size = (3 * buflen + 1) >> 1;
       char *bytebuf = malloc(size);
       if (!buf)
         goto done;
-      WideCharToMultiByte(CP_UTF8, 0, buf, buflen, bytebuf, size, NULL,
-                          NULL);
+      WideCharToMultiByte(CP_UTF8, 0, buf, buflen, bytebuf, size, NULL, NULL);
       fwrite(bytebuf, 1, size, stderr);
       fputs("\n", stderr);
       fflush(stderr);
@@ -134,9 +133,7 @@ static OsHandle create_directory_and_file(struct StringBuf *path) {
   abort(); // impossible
 }
 
-static int sliprock_fsync(OsHandle fd) {
-  return FlushFileBuffers(fd) ? 0 : -1;
-}
+static int sliprock_fsync(OsHandle fd) { return FlushFileBuffers(fd) ? 0 : -1; }
 
 #if 0
 static int get_errno(void) {
@@ -155,16 +152,16 @@ static int set_errno(DWORD err) {
 #define T(x) (L##x)
 
 static wchar_t *CopyIdent(const char *identifier, const size_t size) {
-  int res = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, identifier,
-                                size, NULL, 0);
+  int res = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, identifier, size,
+                                NULL, 0);
   wchar_t *identifier_;
   if (0 == res)
     return NULL;
   identifier_ = calloc(res, sizeof(wchar_t));
   if (identifier_ == NULL)
     return NULL;
-  if (0 == MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, identifier,
-                               size, identifier_, (size_t)res)) {
+  if (0 == MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, identifier, size,
+                               identifier_, (size_t)res)) {
     free(identifier_);
     return NULL;
   }
@@ -191,8 +188,6 @@ static int write_connection(OsHandle fd, struct SliprockConnection *con) {
     return -1;
   if (!FlushFileBuffers(fd))
     return -1;
-  /* FIXME there must be a better way to synchronize the directory update */
-  Sleep(10);
   return 0;
 }
 static HANDLE
@@ -210,10 +205,8 @@ sliprock_get_handle_for_connection(struct SliprockConnection *connection) {
   SetLastError(0);
   connection->hPipe = CreateNamedPipeW(
       connection->pipename,
-      PIPE_ACCESS_DUPLEX |
-          (NULL == hPipe ? FILE_FLAG_FIRST_PIPE_INSTANCE : 0),
-      PIPE_TYPE_MESSAGE | PIPE_REJECT_REMOTE_CLIENTS,
-      PIPE_UNLIMITED_INSTANCES,
+      PIPE_ACCESS_DUPLEX | (NULL == hPipe ? FILE_FLAG_FIRST_PIPE_INSTANCE : 0),
+      PIPE_TYPE_MESSAGE | PIPE_REJECT_REMOTE_CLIENTS, PIPE_UNLIMITED_INSTANCES,
       1U << 12, /* Small to preserve nonpaged pool */
       1U << 12, 0, &sec_attributes);
   connection->lastError = GetLastError();
@@ -226,10 +219,9 @@ retry:
   if (sliprock_randombytes_sysrandom_buf(random, sizeof random) < 0)
     return -1;
   assert(connection->pipename);
-  int res = swprintf_s(connection->pipename,
-                       sizeof connection->pipename / sizeof(wchar_t),
-                       L"\\\\?\\pipe\\SlipRock\\%d\\%I64x",
-                       GetCurrentProcessId(), random[0]);
+  int res = swprintf_s(
+      connection->pipename, sizeof connection->pipename / sizeof(wchar_t),
+      L"\\\\?\\pipe\\SlipRock\\%d\\%I64x", GetCurrentProcessId(), random[0]);
   if (res == -1) {
     assert(0);
     abort();
@@ -247,12 +239,22 @@ retry:
 
 SLIPROCK_API int sliprock_accept(struct SliprockConnection *connection,
                                  SliprockHandle *handle) {
-  sliprock_trace(
-      "sliprock_accept() called.  Getting connection handle.\n");
-  HANDLE hPipe = sliprock_get_handle_for_connection(connection);
+  /*
+   * It is absolutely imperative that there be ONLY ONE instance of the named
+   * pipe open but not connected when ConnectNamedPipe() is called.  Otherwise,
+   * Windows will pick an arbitrary one.  This causes ConnectNamedPipe() to hang
+   * intermittently, causing problems that are terribly difficult to debug!
+   *
+   * The solution is to wait until ConnectNamedPipe() returns to make the next
+   * CreateNamedPipe() call.  This bug took a very long time to fix.
+   */
+  DWORD written_this_time, written = 0;
+  HANDLE hPipe = connection->hPipe;
   int err;
+  sliprock_trace("sliprock_accept() called.  Getting connection handle.\n");
   if (hPipe == INVALID_HANDLE_VALUE) {
     sliprock_trace("Invalid handle!\n");
+    SetLastError(connection->lastError);
     sliprock_strerror();
     return SLIPROCK_EOSERR;
   }
@@ -266,10 +268,11 @@ SLIPROCK_API int sliprock_accept(struct SliprockConnection *connection,
     goto fail;
   }
   sliprock_trace("ConnectNamedPipe() succeeded\n");
-  DWORD written_this_time, written = 0;
+  sliprock_get_handle_for_connection(connection);
   MADE_IT;
   while (1) {
     MADE_IT;
+    sliprock_trace("About to write to pipe\n");
     if (WriteFile(hPipe, connection->passwd + written,
                   sizeof connection->passwd - written, &written_this_time,
                   NULL) == 0) {
@@ -310,8 +313,10 @@ static DWORD sliprock_read_all(HANDLE hnd, void *buf, DWORD size) {
   char *buf_ = buf;
   DWORD read;
   do {
+    MADE_IT;
     if (!ReadFile(hnd, buf_, size, &read, NULL))
       break;
+    MADE_IT;
     if (0 == read)
       break;
     if (read > size)
@@ -330,6 +335,8 @@ static ssize_t sliprock_read_receiver(OsHandle fd,
   DWORD read;
   SLIPROCK_STATIC_ASSERT(MAGIC_SIZE == sizeof SLIPROCK_MAGIC - 1);
   read = sliprock_read_all(fd, buf, sizeof buf);
+  sliprock_trace("Read from connection file\n");
+  MADE_IT;
   if (read != sizeof buf) {
 #ifdef SLIPROCK_TRACE
     fprintf(stderr, "Read %lu bytes - expected %Iu\n", read, sizeof buf);
@@ -352,17 +359,15 @@ SLIPROCK_API int sliprock_connect(const struct SliprockReceiver *receiver,
   HANDLE hPipe = CreateFileW(
       receiver->sock, GENERIC_READ | GENERIC_WRITE,
       FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL,
-      OPEN_EXISTING,
-      SECURITY_SQOS_PRESENT | SECURITY_ANONYMOUS,
-      NULL);
-  sliprock_trace("Created file\n");
+      OPEN_EXISTING, SECURITY_SQOS_PRESENT | SECURITY_ANONYMOUS, NULL);
+  sliprock_trace("Created file handle\n");
   *handle = (SliprockHandle)INVALID_HANDLE_VALUE;
   if (INVALID_HANDLE_VALUE == hPipe)
     return SLIPROCK_EOSERR;
   sliprock_strerror();
   MADE_IT;
-  if ((read = sliprock_read_all(hPipe, pass, sizeof pass)) !=
-      sizeof pass) {
+  sliprock_trace("About to read from pipe\n");
+  if ((read = sliprock_read_all(hPipe, pass, sizeof pass)) != sizeof pass) {
 #ifdef SLIPROCK_TRACE
     sliprock_strerror();
     fprintf(stderr, "Protocol malfunction! Read %lu bytes, expected %Iu\n",
@@ -372,8 +377,8 @@ SLIPROCK_API int sliprock_connect(const struct SliprockReceiver *receiver,
     err = SLIPROCK_EPROTO;
     goto fail;
   }
-  if (sliprock_secure_compare_memory(pass, receiver->passcode,
-                                     sizeof pass)) {
+  sliprock_trace("Read from pipe succeeded\n");
+  if (sliprock_secure_compare_memory(pass, receiver->passcode, sizeof pass)) {
     err = SLIPROCK_ENOAUTH;
     goto fail;
   }
