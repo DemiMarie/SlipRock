06fef7cfff100c1375552500f8369fec60ef087f
Demi Obenour <demiobenour@gmail.com>
2017-07-16 14:44:02 -0400

    Use cmd.exe instead of PowerShell for tests
    It avoids needing to spawn a separate cmd process for each test.

--- test.bat
+++ test.bat
@@ -1,8 +1,19 @@
 cd "%~dp0"
 setlocal enabledelayedexpansion
-set limit=3
+set limit=0
+if !CONFIGURATION!=="Debug" set limit=3
 if !CONFIGURATION!=="Release" set limit=100
-for /L %%x in (0, 1, !CONFIGURATION!) do (
+if !limit!==0 goto bad
+for /L %%x in (1, 1, !limit!) do (
    echo:Run number %%x
-   "src\!CONFIGURATION!\mytest.exe" 2>&1
+   "src\!CONFIGURATION!\mytest.exe"
+   if errorlevel 1 goto fail
 )
+goto :eof
+:bad
+echo:Bad value for %%CONFIGURATION%%
+exit /b 1
+:fail
+set q=!ERRORLEVEL!
+echo:Test failed with error !q!
+exit /b !q!
