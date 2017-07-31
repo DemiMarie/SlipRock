61d796c56bec6c9326cd200bc684129a22e032ed
Demi Obenour <demiobenour@gmail.com>
2017-07-30 20:24:25 -0400

    Disable echo in cmd.exe

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
+@echo off
 cd "%~dp0"
 setlocal enabledelayedexpansion
 set errorlevel=
@@ -5,7 +6,7 @@ set limit=
 if !CONFIGURATION!==Debug set limit=3
 if !CONFIGURATION!==Release set limit=100
 if !limit!==0 goto bad
-if not !ERRORLEVEL!==0 goto bad
+if not !errorlevel!==0 goto bad
 echo.Config is !CONFIGURATION!
 for /L %%x in (1, 1, !limit!) do (
    echo.Run number %%x
@@ -17,6 +18,6 @@ goto :eof
 echo.Bad value for %%CONFIGURATION%%
 exit /b 1
 :fail
-set q=!ERRORLEVEL!
+set q=!errorlevel!
 echo.Test failed with error !q!
 exit /b !q!
